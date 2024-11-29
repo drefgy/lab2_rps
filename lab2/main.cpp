316dfdb1439145db0dc2a2ec2811e95c132045eb
drefgy <mainyakina@yandex.ru>
2024-11-29 12:24:28 +0300

    Initial Commit

---
+++ lab2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "menu.hpp"
+
+using namespace std;
+
+int main() {
+    
+    greeting();
+    showMenu();
+    
+    return 0;
+}
---
+++ lab2/menu.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <locale>
+#include <vector>
+
+void greeting();
+void showMenu();
+
