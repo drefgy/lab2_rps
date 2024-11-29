46713f6c54be9f5b53e56c1f5ee9b4eedc5a3593
wormwoodb <enot270773@gmail.com>
2024-11-29 12:53:17 +0300

    Add files via upload
    Добавление файлов, реализующих проверку на ввод и функции для корректной работы программы

---
+++ lab2/functions.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+void shellSort(vector<int>& array);
+void inputArray(vector<int>& array);
+void printArray(vector<int>& array);
+void randomArray(vector<int>& array);
+void outputData(vector<int> array);
+void saveArrayToFile(const vector<int>& array, const string& filePath);
---
+++ lab2/inputValidation.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#define upperBound 100
+#define lowerBound -100
+/* */
+int inputInt();
+double inputDouble();
+bool inputBool();
+unsigned long inputUnsignedLong();
+
